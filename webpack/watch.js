85696b4997d80f4b88413aa0697011cd21400b65
Kristoffer95 <Kristoffer.gonzaga@urios.edu.ph>
2019-01-31 16:43:30 +0800

    ->Properties for the Parent (flex container)       flex-direction: [fxd] (ADDED)         - row [r]         - row-reverse [rr]         - column [c]         - column-reverse [cr]
          flex-wrap: [fxw] (ADDED)
            - nowrap [nw]
            - wrap [w]
            - wrap-reverse [wr]
    
          flex-flow: [fxf] (ADDED)
            - flex-direction [fxd]
            - flex-wrap [fxw]
    
          justify-content: [jc] (ADDED)
            - flex-start [fxs]
            - flex-end [fxe]
            - center [c]
            - space-between [sb]
            - space-around [sa]
            - space-evenly [se]
    
          align-items: [ai] (ADDED)
            - flex-start [fxs]
            - flex-end [fxe]
            - center [c]
            - baseline [b]
            - stretch [s]
    
          align-content: [ac] (ADDED)
            - flex-start [fxs]
            - flex-end [fxe]
            - center [c]
            - space-between [sb]
            - space-around [sa]
            - stretch [s]
    
      -> Properties for the Children (flex items)
    
          order: [or] (ADDED)
            - [<integer>; /* default is 0 */]
    
          flex-grow: [fxg] (ADDED)
            - [<number>; /* default 0 */]
    
          flex-shrink: [fxs] (ADDED)
            - [<number>; /* default 1 */]

--- webpack/watch.js
+++ webpack/watch.js
@@ -1,15 +1,20 @@
 require("./paint.js");
+// this will automatic run function
+// because function is inside the parentheses. ex. "( fn )"
 (function(){
+  // watch all classes
   cssist.watch = {
     change : function(element){
       element.addEventListener('DOMNodeInserted', function(event){
         cssist.paint.rootElement(event.target);
       }, false);
     },
+
     start : function(){
       var self = this;
       var elements;
       var interval = setInterval(function () {
+        // count all classes
         if( document.querySelectorAll('[cssist]').length>=1 ){
           elements = document.querySelectorAll('[cssist]');
         }
